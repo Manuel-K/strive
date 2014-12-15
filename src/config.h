b36a1a91742ffdb93601d1f6ec71cb00423c95f0
Mike Swanson <mikeonthecomputer@gmail.com>
2014-12-14 21:23:41 -0800

    src/config.h: add some include guards

--- src/config.h
+++ src/config.h
@@ -1,4 +1,9 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
 #define PACKAGE_NAME "strife-ve"
 #define PACKAGE_STRING "Strife: Veteran Edition"
 #define PACKAGE_TARNAME "strife-ve"
 #define PROGRAM_PREFIX "strife-ve-"
+
+#endif
